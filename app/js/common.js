f84449dcfd5cb33c57897caff26bf4c2c46fbc54
AlexSlet <lekhasleta95@gmail.com>
2017-11-10 18:54:44 +0200

    Script

--- app/js/common.js
+++ app/js/common.js
@@ -1,5 +1,8 @@
 $(function() {
 
-	// Custom JS
+	$(document).on("click", ".form__btn", function(){
+		var comment = $("#textarea").val();
+		$("<div class='bodyComment'>" + comment + "</div>").appendTo(".lastFeedback__field");
+	});
 
 });
